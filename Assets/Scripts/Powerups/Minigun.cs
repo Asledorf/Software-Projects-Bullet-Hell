468d928cf034d77b2c0775ce23477c4c24a4fe51
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2021-11-19 19:18:21 -0700

    Made minigun & tripleShot

---
+++ Assets/Scripts/Powerups/Minigun.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigun : MonoBehaviour
+{
+    public float shoot_speed = 100;
+    public GameObject bullet_prefab = null;
+    float fire_rate = 0.05f;
+    float accumulator = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if ((accumulator += Time.deltaTime) >= fire_rate)
+        {
+            Shoot();
+            accumulator = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 shoot_direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow)) shoot_direction += Vector3.up;
+        if (Input.GetKey(KeyCode.DownArrow)) shoot_direction += Vector3.down;
+        if (Input.GetKey(KeyCode.LeftArrow)) shoot_direction += Vector3.left;
+        if (Input.GetKey(KeyCode.RightArrow)) shoot_direction += Vector3.right;
+
+        if (shoot_direction != Vector3.zero & bullet_prefab)
+        {
+            GameObject go = Instantiate(bullet_prefab, transform.position, Quaternion.identity, null);
+            go.GetComponent<bullet>().move = shoot_direction.normalized * shoot_speed;
+            Destroy(go, 3);
+        }
+    }
+}
