1f0e65b9ddca5a76f7869ce8b0a77dd1767d1c66
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2021-12-01 20:47:38 -0700

    Fixed Errors resulting in having to enter safe mode

--- Assets/Scripts/Powerups/Pinwheel.cs
+++ Assets/Scripts/Powerups/Pinwheel.cs
@@ -12,14 +12,6 @@ public class Pinwheel : MonoBehaviour
     public float lifeTime = 5f;
     private float timer = 0;
 
-    private void Start()
-    {
-        if (!bullet_prefab)
-        {
-            bullet_prefab = gameObject.GetComponent<Player>().bulletPrefab;
-        }
-    }
-
     private void OnEnable()
     {
         timer = 0;
--- Assets/Scripts/Powerups/TripleShot.cs
+++ Assets/Scripts/Powerups/TripleShot.cs
@@ -14,14 +14,6 @@ public class TripleShot : MonoBehaviour
     public float lifeTime = 5f;
     private float timer = 0;
 
-    private void Start()
-    {
-        if (!bullet_prefab)
-        {
-            bullet_prefab = gameObject.GetComponent<Player>().bulletPrefab;
-        }
-    }
-
     private void OnEnable()
     {
         timer = 0;
