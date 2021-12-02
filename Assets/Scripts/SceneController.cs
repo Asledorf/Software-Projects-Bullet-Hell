32b60ed8771f64bb773039f7eb5469edff5c43b4
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2021-12-01 18:15:08 -0700

    Main Menu & Options Menu done
    Just need to get the audio mixer done and the pause menu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    class SceneController : MonoBehaviour
+    {
+        public bool soundToMenu = true;
+        static SceneController instance = null;
+        public static SceneController Instance { get { return instance; } }
+     
+        public void Awake()
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        public void OnLoadScene(string sceneString)
+        {
+            StartCoroutine(LoadScene(sceneString));
+        }
+
+        IEnumerator LoadScene(string sceneString)
+        {
+            SceneManager.LoadScene(sceneString);
+
+            yield return null;
+        }
+    }
+}
